127060cb2227f7dbb7110376fe1936be57ee2b1b
elloot <elloot@github.com>
2019-12-12 11:19:33 +0100

    start work on day3

---
+++ src/day3.java
@@ -0,0 +1,12 @@
+/**
+ * Finds the closest intersection between cables
+ * 2019-12-12
+ * Author: Elliot Duchek
+ */
+public class day3 {
+    public static void main(String[] args) {
+        int cabAX, cabAY, cabBX, cabBY = 0;
+
+
+    }
+}
