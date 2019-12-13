903bd4126439c534b65650fbc6c0c8a73956c365
elloot <elloot@github.com>
2019-12-13 11:21:09 +0100

    lay groundwork for day 3, need to fix for loop

--- src/day3.java
+++ src/day3.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Finds the closest intersection between cables
  * 2019-12-12
@@ -5,7 +10,34 @@
  */
 public class day3 {
     public static void main(String[] args) {
-        int cabAX, cabAY, cabBX, cabBY = 0;
+        ArrayList<Integer> cableAX = new ArrayList<Integer>();
+        cableAX.add(0);
+        ArrayList<Integer> cableAY = new ArrayList<Integer>();
+        cableAY.add(0);
+
+        Scanner in = null;
+
+        try {
+            in = new Scanner (new File("./input/inputDay3"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            in = new Scanner(System.in);
+        }
+
+        String tempIn = in.nextLine();
+        String[] moveA = tempIn.split(",");
+
+        for (int i = 0; i<moveA.length; i++) {
+            String tempMove = moveA[i];
+
+            if (tempMove.charAt(0) == 'R') {
+                int tempMoveNumber = Integer.parseInt(tempMove.substring(1));
+                for (int j = 0; j < tempMoveNumber; j++) {
+                    int last = cableAX.get(cableAX.size() - 1);
+                    cableAX.add(j+last);
+                }
+            }
+        }
 
 
     }
